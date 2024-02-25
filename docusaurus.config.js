0097854024c5e1df93ae3de39eccfb16005e00e8
d479 <daniel.madureira@ua.pt>
2024-02-25 18:32:29 +0000

    tweak configs

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -66,9 +66,9 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: 'Distribuição de Serviço Docente',
         logo: {
-          alt: 'My Site Logo',
+          alt: 'dsd_logo',
           src: 'img/logo.svg',
         },
         items: [
