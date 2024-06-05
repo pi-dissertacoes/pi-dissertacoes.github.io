f2f5954f3916a0dcfe4b99cce68dd3e51b41b00d
d479 <daniel.madureira@ua.pt>
2024-06-06 00:32:29 +0100

    Revamped hero

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,36 +1,50 @@
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import clsx from "clsx";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import Layout from "@theme/Layout";
+import HomepageFeatures from "@site/src/components/HomepageFeatures";
 
-import Heading from '@theme/Heading';
-import styles from './index.module.css';
+import Heading from "@theme/Heading";
+import styles from "./index.module.css";
+import logo_big from "../../static/img/icons/logo_min.svg";
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
+    <header className={clsx("hero hero--primary", styles.heroBanner)}>
+      <Hero />
+      {/*   <div className="container">
         <Heading as="h1" className="hero__title">
           {siteConfig.title}
         </Heading>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
-      </div>
+      </div> */}
     </header>
   );
 }
 
 export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
-    <Layout
-      description="Description will go into a meta tag in <head />">
-
+    <Layout description="Description will go into a meta tag in <head />">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
       </main>
     </Layout>
+  );
+}
+
+function Hero() {
+  const { siteConfig } = useDocusaurusContext();
+
+  return (
+    <section className={styles.section}>
+      <img src="img/icons/logo_big.svg" alt="Logo" className={styles.logo} />
+      <div className={styles.div}>
+        <Heading as="h1" className="hero__title">
+          {siteConfig.title}
+        </Heading>
+        <p className={clsx("hero__subtitle", styles.p)} >{siteConfig.tagline}s</p>
+      </div>
+    </section>
   );
 }
